587086091e4d913440981bfdecd902fc71cb217b
Jiacheng <ljc050414@gmail.com>
2024-09-25 19:48:37 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,30 @@
+import java.io.*;
+
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
     Polynomial p = new Polynomial();
     System.out.println(p.evaluate(3));
-    double [] c1 = {6,0,0,5};
-    Polynomial p1 = new Polynomial(c1);
-    double [] c2 = {0,-2,0,0,-9};
-    Polynomial p2 = new Polynomial(c2);
+    double [] c1 = {6,5};
+    int[] e1 = {0, 1};
+    Polynomial p1 = new Polynomial(c1,e1);
+    double [] c2 = {-2,-9};
+    int[] e2 = {0, 1};
+    Polynomial p2 = new Polynomial(c2,e2);
     Polynomial s = p1.add(p2);
-    System.out.println("s(0.1) = " + s.evaluate(0.1));
-    if(s.hasRoot(1))
-    System.out.println("1 is a root of s");
-    else
-    System.out.println("1 is not a root of s");
+    Polynomial s2 = p1.multiply(p2);
+    s.printPolynomial();
+    s2.printPolynomial();
+    Polynomial s3 = new Polynomial(new File("a.txt"));
+    s3.printPolynomial();
+    p2.saveToFile("p2.txt");
+
+    
+    
+
+    // System.out.println("s(0.1) = " + s.evaluate(0.1));
+    // if(s.hasRoot(1))
+    // System.out.println("1 is a root of s");
+    // else
+    // System.out.println("1 is not a root of s");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,178 @@
 import java.lang.Math;
+import java.io.*;
+import java.util.*;
 
 public class Polynomial {
 
     private double[] coefficient;
+    private int[] exp;
 
     public Polynomial() {
-        coefficient = new double[1];
-        coefficient[0] = 0;
+        this.coefficient = new double[0];
+        this.exp = new int[0];
     }
 
-    public Polynomial(double[] coefficient) {
+    public Polynomial(double[] coefficient, int[] exp) {
         this.coefficient = coefficient;
+        this.exp = exp;
+    }
+
+    public Polynomial(File file) throws FileNotFoundException {
+        this.coefficient = new double[0];
+        this.exp = new int[0];
+        String poly = new String();
+        Scanner fileScanner = new Scanner(file);
+        poly = fileScanner.nextLine();
+        String element = new String();
+        for (int i = 0; i < poly.length(); i++) {
+            if ((poly.charAt(i) == '-' && i != 0) || poly.charAt(i) == '+' || i == poly.length() - 1) {
+                if (i == poly.length() - 1) {
+                    element += poly.charAt(i);
+                }
+                // if (!element.equals("")) {
+                    String[] CnE = element.split("x");
+                    double[] c = new double[this.coefficient.length + 1];
+                    System.arraycopy(this.coefficient, 0, c, 0, this.coefficient.length);
+                    if (CnE.length == 0) {
+                        c[this.coefficient.length] = 1;
+                    } else if (CnE[0].equals("")) {
+                        c[this.coefficient.length] = 1;
+                    } else if (CnE[0].equals("-")){
+                        c[this.coefficient.length] = -1;
+                    } else {
+                        c[this.coefficient.length] = Double.parseDouble(CnE[0]);
+                    }
+                    this.coefficient = c;
+                    int[] e = new int[this.exp.length + 1];
+                    System.arraycopy(this.exp, 0, e, 0, this.exp.length);
+                    if (CnE.length >= 2) {
+                        e[this.exp.length] = Integer.parseInt(CnE[1]);
+                    } else if (element.indexOf("x") != -1){
+                        e[this.exp.length] = 1;
+                    } else {
+                        e[this.exp.length] = 0;
+                    }
+                    this.exp = e;
+                    element = new String();
+                
+                if (poly.charAt(i) == '-') {
+                    element += poly.charAt(i);
+                }
+            } else {
+                element += poly.charAt(i);
+            }
+        }
+        fileScanner.close();
     }
 
     public Polynomial add(Polynomial other) {
-        int len = this.coefficient.length > other.coefficient.length ? this.coefficient.length : other.coefficient.length;
-        double[] resultC = new double[len];
-        for (int i = 0; i < len; i++) {
-            resultC[i] = 0;
+        double[] resultC = this.coefficient.clone();
+        int[] resultE = this.exp.clone();
+        for (int i = 0; i < other.exp.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < this.exp.length; j++) {
+                if (other.exp[i] == this.exp[j]) {
+                    flag = false;
+                    resultC[j] += other.coefficient[i];
+                }
+            }
+            if (flag) {
+                double[] c = new double[resultC.length + 1];
+                System.arraycopy(resultC, 0, c, 0, resultC.length);
+                c[resultC.length] = other.coefficient[i];
+                int[] e = new int[resultE.length + 1];
+                System.arraycopy(resultE, 0, e, 0, resultE.length);
+                e[resultE.length] = other.exp[i];
+                resultC = c;
+                resultE = e;
+            }
         }
-        for (int i = 0; i < this.coefficient.length; i++) {
-            resultC[i] += this.coefficient[i];
-        }
-        for (int i = 0; i < other.coefficient.length; i++) {
-            resultC[i] += other.coefficient[i];
-        }
-        Polynomial result = new Polynomial(resultC);
+        Polynomial result = new Polynomial(resultC, resultE);
         return result;
     }
 
     public double evaluate(double x) {
         double result = 0;
         for (int i = 0; i < this.coefficient.length; i++) {
-            result += this.coefficient[i] * Math.pow(x, i);
+            result += this.coefficient[i] * Math.pow(x, this.exp[i]);
         }
         return result;
     }
 
     public boolean hasRoot(double x) {
         return this.evaluate(x) == 0;
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        Polynomial result = new Polynomial();
+        for (int i = 0; i < this.coefficient.length; i++) {
+            for (int j = 0; j < other.coefficient.length; j++) {
+                int[] resultE = new int[1];
+                double[] resultC = new double[1];
+                resultE[0] = this.exp[i] + other.exp[j];
+                resultC[0] = this.coefficient[i] * other.coefficient[j];
+                Polynomial resultP = new Polynomial(resultC, resultE);
+                result = result.add(resultP);
+            }
+        }
+        return result;
+    }
+
+    public void saveToFile(String fileName) throws IOException{
+        String result = new String();
+        for (int i = 0; i < this.coefficient.length; i++) {
+            if (this.coefficient[i] > 0 && i != 0) {
+                result += "+";
+            }
+            if (this.coefficient[i] == 1) {
+                result += "";
+            } else if (this.coefficient[i] == -1) {
+                result += "-";
+            } else if (this.coefficient[i] == (int)this.coefficient[i]) {
+                result += (int)this.coefficient[i];
+            } else {
+                result += this.coefficient[i];
+            }
+            if (this.exp[i] == 0) {
+                result += "";
+            } else if (this.exp[i] == 1) {
+                result += "x";
+            } else {
+                result += "x";
+                result += this.exp[i];
+            }
+        }
+        File file = new File(fileName);
+        file.createNewFile();
+        FileWriter writer = new FileWriter(file);
+        writer.write(result);
+        writer.close();
+    }
+
+    public void printPolynomial() {
+        String result = new String();
+        for (int i = 0; i < this.coefficient.length; i++) {
+            if (this.coefficient[i] > 0 && i != 0) {
+                result += "+";
+            }
+            if (this.coefficient[i] == 1) {
+                result += "";
+            } else if (this.coefficient[i] == -1) {
+                result += "-";
+            } else if (this.coefficient[i] == (int)this.coefficient[i]) {
+                result += (int)this.coefficient[i];
+            } else {
+                result += this.coefficient[i];
+            }
+            if (this.exp[i] == 0) {
+                result += "";
+            } else if (this.exp[i] == 1) {
+                result += "x";
+            } else {
+                result += "x";
+                result += this.exp[i];
+            }
+        }
+        System.out.println(result);
     }
 }
